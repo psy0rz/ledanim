770c72ca694b68c01a604d8a5e43a5fc0b587a3b
Edwin Eefting <edwin@datux.nl>
2016-09-12 22:58:57 +0200

    make commands settable from javascript

--- web/strip_anim_bindings.cpp
+++ web/strip_anim_bindings.cpp
@@ -1,7 +1,9 @@
 #include <emscripten/bind.h>
 
+#define DEBUG_OUTPUT
 #include "strip_anim.hpp"
 #include "crgb.h"
+#include <stdint.h>
 
 #define MAX_LEDS 200
 
@@ -15,12 +17,13 @@ EMSCRIPTEN_BINDINGS(strip_anim_bindings) {
         .field("b", &CRGB::b)
         ;
 
+    emscripten::register_vector<uint8_t>("commands_t");
 
 
     emscripten::class_<strip_anim_c<MAX_LEDS>  >("strip_anim_c")
         .constructor<>()
         .function("step", &strip_anim_c<MAX_LEDS>::step)
         .function("get_led", &strip_anim_c<MAX_LEDS>::get_led)
+        .function("set_commands", &strip_anim_c<MAX_LEDS>::set_commands)
         ;
-
 }
