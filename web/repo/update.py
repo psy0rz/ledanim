2ebc332dc08231f1ae8a036593838f5e6ca590cc
Edwin Eefting <edwin@datux.nl>
2016-09-24 23:32:57 +0200

    repo stuff

--- web/repo/update.py
+++ web/repo/update.py
@@ -2,13 +2,13 @@
 
 import os
 import json
+from collections import OrderedDict
 
-repo={}
+repo=OrderedDict()
 for (path, dirs, files) in os.walk("."):
     category=path[2:]
     if category:
-        if not category in repo:
-            repo[category]={}
+        repo[category]=OrderedDict()
         for filename in files:
             with open(path+"/"+filename) as fh:
                 desc=fh.readline()
@@ -17,6 +17,11 @@ for (path, dirs, files) in os.walk("."):
                 else:
                     desc=""
                 repo[category][filename]=desc
+
+        repo[category]=sorted(repo[category].items())
+
+
+repo=sorted(repo.items())
 
 with open("index.json",'w') as fh:
     fh.write(json.dumps(repo))
