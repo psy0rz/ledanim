4eea3c1abafcb40c075cbeac288741238e0cbfa7
Edwin Eefting <edwin@datux.nl>
2016-10-07 23:13:08 +0200

    oops should be red instead of yellow :P

--- esp8266/config.h
+++ esp8266/config.h
@@ -4,7 +4,7 @@
 #define PIN_POWER_ON    D0
 
 //look in the fastled documentation for your ledstrip and config
-//we're using: FastLED.addLeds< LPD8808, 7, 5, GRB >
+//we're using: FastLED.addLeds< LPD8808, DATA_PIN, CLOCK_PIN, RGB >
 #define FASTLED_CONFIG LPD8806, 7, 5, GRB
 
 //if you're using a nodemcu esp8266, like me
