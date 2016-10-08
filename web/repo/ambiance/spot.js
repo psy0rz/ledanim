6232563743f2945ef1ad948335cce0f0812d974a
Edwin Eefting <edwin@datux.nl>
2016-10-08 23:31:16 +0200

    update

--- web/repo/ambiance/spot.js
+++ web/repo/ambiance/spot.js
@@ -7,19 +7,35 @@ width=control.slider({
     default: 3
 });   
 
-offset=control.slider({
-    name:"Offset between spots", 
+distance=control.slider({
+    name:"Distance between spots", 
     min:1,  
     max:50,
-    default: 10
+    default: 20
 });   
 
 
+offset=control.slider({
+    name:"Starting offset", 
+    min:0,  
+    max:50,
+    default: 0
+});   
+
+
+brightness=control.slider({
+    name:"Brightness", 
+    min:0,  
+    max:255,
+    default: 200
+});   
+
+led.color(brightness, brightness, brightness);
 led.width(width);
-led.mirror(offset);
+led.mirror(distance);
 
 
-led.led(0);
+led.led(offset);
 led.draw();
 
 
