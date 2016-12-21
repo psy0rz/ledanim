ffc53f1affcb4fb1500340deaff3589badb2c15c
Edwin Eefting <edwin@datux.nl>
2016-12-21 17:47:03 +0100

    effects

---
+++ web/repo/xmas/comet.js
@@ -0,0 +1,44 @@
+// Some kind of red green comet :)
+
+tail=30;
+mirror=60;
+
+led.mirror(mirror);
+led.fade_mode(1);
+led.step(0);
+
+for(i=0;i<mirror; i++)
+{
+    //white dot
+    led.led(i);
+    led.set(255,255,255);
+
+    //fade to green fast
+    if (i%2==0)
+        led.color_rnd(0,0, 50,180, 0,0);
+    else 
+        led.color_rnd(50,180,0,0, 0,0);
+
+        led.fade_speed_rnd(10,25);
+        led.draw();
+
+    //fade to black slower at a later point
+    if (i>=tail)
+    {
+        led.led(i-tail);
+    }
+    else
+    {
+        led.led(i-tail+mirror);
+        
+    }
+    led.color(0,0,0);
+    led.fade_speed_rnd(2,3);
+    led.draw();
+
+    
+    
+    led.delay(4);    
+    
+}
+
---
+++ web/repo/xmas/redgreensparkle.js
@@ -0,0 +1,53 @@
+// red green xmas sparkles
+
+delay=control.slider({
+    name:"delay",
+    min:0,  
+    max:100,
+    default: 20
+});   
+
+flashcolor=control.color({
+    name:"Flash color", 
+    default: { r:255, g: 255, b: 255}
+});
+
+
+color1=control.color({
+    name:"Color 1", 
+    default: { r:255, g: 0, b: 0}
+});
+
+
+color2=control.color({
+    name:"Color 2", 
+    default: { r:0, g: 255, b: 0}
+});
+
+/// flash first led color 
+led.led_rnd(0, led.leds);
+led.set(color1.r, color1.g, color1.b)
+led.color(flashcolor.r, flashcolor.g, flashcolor.g);
+led.fade_mode(2);
+led.fade_speed(20);
+led.draw();
+
+led.repeat_begin_rnd(0,delay)
+led.delay(1)
+led.repeat_end();
+
+
+/// flash second led color
+led.led_rnd(0, led.leds);
+led.set(color2.r, color2.g, color2.b)
+led.color(flashcolor.r, flashcolor.g, flashcolor.g);
+led.fade_mode(2);
+led.fade_speed(20);
+led.draw();
+
+led.repeat_begin_rnd(0,delay)
+led.delay(1)
+led.repeat_end();
+
+
+        
